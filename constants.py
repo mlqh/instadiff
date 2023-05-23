494e965d657eeea9df132c177ccc4fc1fed01f71
mlqh <matt.lqh02@gmail.com>
2023-05-23 18:26:55 -0400

    WIP

---
+++ constants.py
@@ -0,0 +1,5 @@
+EXTRACTED_PATH = "./Extracted Data"
+FOLLOWERS_PATH = EXTRACTED_PATH + "/followers_and_following/followers_1"
+FOLLOWING_PATH = EXTRACTED_PATH + "/followers_and_following/following"
+JSON = ".json"
+HTML = ".html"
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+import constants
 import zipfile
 import ujson
-    
+  
 def unzip(path):
     with zipfile.ZipFile(path, 'r') as zip_ref:
-        zip_ref.extractall("./Extracted Data")
+        zip_ref.extractall(constants.EXTRACTED_PATH)
 
 def main():
-    print("Enter zipped file name:")
+    extensions = [constants.HTML, constants.JSON]
+    
+    print("Enter zipped file path: ")
     filename = input()
+    print("Enter data type (HTML/JSON): ")
+    data_type = 0 if input().lower() == "html" else 1
     unzip(filename)
+    followers = open(constants.FOLLOWERS_PATH + extensions[data_type])
+    following = open(constants.FOLLOWING_PATH + extensions[data_type])
+    
+    followers_dict = ujson.load(followers)
+    print("hi")
     
     
 if __name__ == "__main__":
